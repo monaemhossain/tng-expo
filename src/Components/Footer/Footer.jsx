7725e9edc7ad4801f70f6fbe51ee1dfbba4cafea
monaemhossain <monaemhs@gmail.com>
2023-10-07 06:32:37 +0300

    added footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,21 @@
+import { BiLogoFacebookCircle, BiLogoInstagram, BiLogoTwitter, BiLogoPinterest, BiLogoYoutube } from "react-icons/bi";
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="bg-neutral-800 text-white py-8">
+            <div className="max-w-[1024px] mx-auto flex items-center justify-between">
+                <div><p>© 2020 Your Company, Inc. All rights reserved.</p></div>
+                <div>
+                    <ul className="flex gap-2">
+                        <li><a href="#"><BiLogoFacebookCircle className="text-2xl"></BiLogoFacebookCircle></a></li>
+                        <li><a href="#"><BiLogoInstagram className="text-2xl"></BiLogoInstagram></a></li>
+                        <li><a href="#"><BiLogoTwitter className="text-2xl"></BiLogoTwitter></a></li>
+                        <li><a href="#"><BiLogoPinterest className="text-2xl"></BiLogoPinterest></a></li>
+                        <li><a href="#"><BiLogoYoutube className="text-2xl"></BiLogoYoutube></a></li>
+                    </ul>
+                    
+                    
+                </div>
+            </div>
         </div>
     );
 };
--- src/Router/Root.jsx
+++ src/Router/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router';
 import NavigationBar from '../Components/Header/Navbar/NavigationBar';
+import Footer from '../Components/Footer/Footer';
 
 const Root = () => {
     return (
         <>
             <NavigationBar></NavigationBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </>
     );
 };
