e98f2119baa67a77f293322e01c31befa4602bfb
monaemhossain <monaemhs@gmail.com>
2023-10-09 19:05:46 +0300

    updated banner

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -5,13 +5,13 @@ import './Banner.css'
 const Banner = () => {
   return (
     <div className="banner min-h-[620px] relative text-gray-200">
-      <div className='max-w-screen-lg mx-auto grid lg:grid-cols-2 grid-cols-1 max-lg:text-center'>
-        <div className='space-y-3' data-aos="fade-up"
+      <div className='max-w-screen-lg mx-auto grid lg:grid-cols-2 grid-cols-1'>
+        <div className='space-y-3 pl-4' data-aos="fade-up"
           data-aos-anchor-placement="center-bottom">
           <h1 className="text-6xl font-bold">
             Biggest Tech and Gaming Event in 2023
           </h1>
-          <ul className='lg:text-left lg:list-disc ml-5 space-y-1'>
+          <ul className='text-left list-disc ml-5 space-y-1'>
             <li>GameTech Fusion: Where Innovation Meets Gaming</li>
             <li>NextGenTech: Unleashing the Future of Gaming</li>
             <li>Digital Playgrounds: A Convergence of Tech and Gaming</li>
